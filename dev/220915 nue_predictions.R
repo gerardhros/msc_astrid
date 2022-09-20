238d66c1bd3382377592515d54beec2efe4e83ac
Astrid Berndsen <astrid.berndsen@nmi-agro.nl>
2022-09-20 10:00:14 +0200

    Minor corrections on predictions

--- dev/220915 nue_predictions.R
+++ dev/220915 nue_predictions.R
@@ -7,7 +7,7 @@ require(metafor);require(data.table);require(ggplot2)
 #rm(list=ls())
 
 # load metafor model: meta2 = rma.mv(fnue,fvnue,mods = ~ xclay + xsom + tmp_mean + pre_mean + factor(n_source)*n_dose + I(n_dose^2),random=~1| no,data=dt2,method="ML", sparse = TRUE)
-meta2 <- readRDS('products/nue_meta2.rds')
+#meta2 <- readRDS('products/nue_meta2.rds')
 
 
 # predict NUE under varying N dose and varying n source
@@ -15,10 +15,10 @@ meta2 <- readRDS('products/nue_meta2.rds')
                                                c(35,35,35,35,35,35,35,35,35,35,35,35),# xsom
                                                c(15,15,15,15,15,15,15,15,15,15,15,15), # tmp_mean
                                                c(1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000), # pre_mean
-                                               c(1,1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0), # nsource(inorganic)
+                                               c(1,1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0), # nsource(inorganic)
                                                c(25,50,75,100,125,150,175,200,225,250,275,300), # n_dose
                                                c(25,50,75,100,125,150,175,200,225,250,275,300), # n_dose^2
-                                               c(1,1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0))) # nsource(inorganic) of c(25,50,75,100,125,150,175,200,225,250,275,300),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0)
+                                               c(25,50,75,100,125,150,175,200,225,250,275,300),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0))) # nsource(inorganic) of c(25,50,75,100,125,150,175,200,225,250,275,300),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0)
                                                
   # bind model estimates together
   
@@ -54,7 +54,7 @@ meta2 <- readRDS('products/nue_meta2.rds')
                                  legend.position = c(0.10,.15), 
                                  axis.text.x = element_text(color="black", size = 11), 
                                  axis.text.y = element_text(color="black", size = 11),
-                                 axis.title = element_text(color="black", size = 15)) + ylim(0,100)
+                                 axis.title = element_text(color="black", size = 15)) + ylim(0,101)
                            
   # save plot
   #ggsave(plot = plot_nue_ndose, filename = 'products/nue_ndose.jpg')
@@ -65,39 +65,49 @@ meta2 <- readRDS('products/nue_meta2.rds')
                                              c(35,35,35,35,35,35,35,35,35,35,35,35),# xsom
                                              c(15,15,15,15,15,15,15,15,15,15,15,15), # tmp_mean
                                              c(1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000), # pre_mean
-                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0), # nsource(efficiency)
+                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0), # nsource(efficiency)
                                              c(25,50,75,100,125,150,175,200,225,250,275,300), # n_dose
                                              c(25,50,75,100,125,150,175,200,225,250,275,300), # n_dose
-                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0))) # nsource(efficiency)
+                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0))) # nsource(efficiency)
                                              
   
   p1_inorg <- predict.rma(meta2, newmods=cbind(c(25,25,25,25,25,25,25,25,25,25,25,25),# xclay
                                                c(35,35,35,35,35,35,35,35,35,35,35,35),# xsom
                                                c(15,15,15,15,15,15,15,15,15,15,15,15), # tmp_mean
                                                c(1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000), # pre_mean
-                                               c(1,1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0), # nsource(inorganic)
+                                               c(1,1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0), # nsource(inorganic)
                                                c(25,50,75,100,125,150,175,200,225,250,275,300), # n_dose
                                                c(25,50,75,100,125,150,175,200,225,250,275,300), # n_dose
-                                               c(1,1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0))) # nsource(inorganic)
+                                               c(25,50,75,100,125,150,175,200,225,250,275,300),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0))) # nsource(inorganic)
                                                
   
   p1_man <- predict.rma(meta2, newmods=cbind(c(25,25,25,25,25,25,25,25,25,25,25,25),# xclay
                                              c(35,35,35,35,35,35,35,35,35,35,35,35),# xsom
                                              c(15,15,15,15,15,15,15,15,15,15,15,15), # tmp_mean
                                              c(1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000), # pre_mean
-                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(1,1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0,0), # nsource(manure)
+                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(1,1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0), # nsource(manure)
                                              c(25,50,75,100,125,150,175,200,225,250,275,300), # n_dose
                                              c(25,50,75,100,125,150,175,200,225,250,275,300), # n_dose
-                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(1,1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0,0))) # nsource(manure)
+                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(25,50,75,100,125,150,175,200,225,250,275,300),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0))) # nsource(manure)
   
   p1_mix <- predict.rma(meta2, newmods=cbind(c(25,25,25,25,25,25,25,25,25,25,25,25),# xclay
                                              c(35,35,35,35,35,35,35,35,35,35,35,35),# xsom
                                              c(15,15,15,15,15,15,15,15,15,15,15,15), # tmp_mean
                                              c(1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000), # pre_mean
-                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(1,1,1,1,1,1,1,1,1,1,1,1), # nsource(mixture)
+                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(1,1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0,0), # nsource(mixture)
                                              c(25,50,75,100,125,2000,175,200,225,250,275,300), # n_dose
                                              c(25,50,75,100,125,2000,175,200,225,250,275,300), # n_dose
-                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(1,1,1,1,1,1,1,1,1,1,1,1))) # nsource(mixture)
+                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(25,50,75,100,125,150,175,200,225,250,275,300),c(0,0,0,0,0,0,0,0,0,0,0,0))) # nsource(mixture)
+  
+  p1_org <- predict.rma(meta2, newmods=cbind(c(25,25,25,25,25,25,25,25,25,25,25,25),# xclay
+                                             c(35,35,35,35,35,35,35,35,35,35,35,35),# xsom
+                                             c(15,15,15,15,15,15,15,15,15,15,15,15), # tmp_mean
+                                             c(1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000), # pre_mean
+                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(1,1,1,1,1,1,1,1,1,1,1,1), # nsource(organic)
+                                             c(25,50,75,100,125,2000,175,200,225,250,275,300), # n_dose
+                                             c(25,50,75,100,125,2000,175,200,225,250,275,300), # n_dose
+                                             c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0),c(25,50,75,100,125,150,175,200,225,250,275,300))) # nsource(organic)
+  
                                              
   
 # bind model estimates together
@@ -107,6 +117,7 @@ meta2 <- readRDS('products/nue_meta2.rds')
   e1_inorg <- as.data.frame(p1_inorg)
   e1_man <- as.data.frame(p1_man)
   e1_mix <- as.data.frame(p1_mix)
+  e1_org <- as.data.frame(p1_org)
 
   # extract vectors 
   
@@ -170,33 +181,47 @@ meta2 <- readRDS('products/nue_meta2.rds')
     
     # combine extractions and vectors
     nue_e1_mix <- data.table(nue,se,cil,ciu,xclay,xsom,tmp,pre,nsource,ndose)
+    
+  # for nsource: organic
+    # extract vectors pred, ci.lb and ci.ub from the model estimates with n source 'organic'
+    e1_org.df <- data.frame(e1_org)
+    nue <- unlist(e1_org.df$pred, use.names=FALSE)
+    cil <- unlist(e1_org.df$ci.lb, use.names=FALSE)
+    ciu <- unlist(e1_org.df$ci.ub, use.names=FALSE)
+    se <- unlist(e1_org.df$se, use.names=FALSE)
+    
+    # create vector for n source
+    nsource <- rep("organic",12)
+    
+    # combine extractions and vectors
+    nue_e1_org <- data.table(nue,se,cil,ciu,xclay,xsom,tmp,pre,nsource,ndose)  
   
   # combine estimates and variables for the five levels of n source
-  nue_e1_all <- rbind(nue_e1_eff,nue_e1_inorg,nue_e1_man,nue_e1_mix)
+  nue_e1_all <- rbind(nue_e1_eff,nue_e1_inorg,nue_e1_man,nue_e1_mix,nue_e1_org)
     
   # visualise the predictions for nue_e1_all 
   plot_nue_source <- ggplot(data=nue_e1_all,aes(x=(ndose),y=nue, color = nsource, group=nsource)) +
                      geom_point() + geom_line(show.legend=FALSE) + theme_bw() +
-                     scale_color_manual(values=c('chartreuse3','deepskyblue','orange','darkolivegreen','blue')) 
+                     scale_color_manual(values=c('chartreuse3','deepskyblue','orange','darkolivegreen','blue')) +
                      xlab("N dose (kg/ha)") + ylab("NUE (%)") +
                      theme(legend.title = element_text(size=10),
                            legend.position = c(0.10,.15), 
                            axis.text.x = element_text(color="black", size = 11), 
                            axis.text.y = element_text(color="black", size = 11),
-                           axis.title = element_text(color="black", size = 15)) + ylim(0,100)
+                           axis.title = element_text(color="black", size = 15)) + ylim(0,120)
   # save plot
   #ggsave(plot = plot_nue_source, filename = 'products/nue_source.jpg')
                      
     
  # predict NUE under varying clay content
- p2_clay <- predict.rma(meta2, newmods=cbind(c(5,10,15,20,25,30,35,40,45,50),# xclay
-                                            c(35,35,35,35,35,35,35,35,35,35),# xsom
-                                            c(15,15,15,15,15,15,15,15,15,15), # tmp_mean
-                                            c(1000,1000,1000,1000,1000,1000,1000,1000,1000,1000), # pre_mean
-                                            c(1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0), # nsource (inorganic)
-                                            c(200,200,200,200,200,200,200,200,200,200), # n_dose
-                                            c(200,200,200,200,200,200,200,200,200,200), # n_dose
-                                            c(1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0))) # nsource (inorganic)
+ p2_clay <- predict.rma(meta2, newmods=cbind(c(5,10,15,20,25,30,35,40,45,50,55),# xclay
+                                            c(35,35,35,35,35,35,35,35,35,35,35),# xsom
+                                            c(15,15,15,15,15,15,15,15,15,15,15), # tmp_mean
+                                            c(1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000), # pre_mean
+                                            c(1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0), # nsource (inorganic)
+                                            c(200,200,200,200,200,200,200,200,200,200,200), # n_dose
+                                            c(200,200,200,200,200,200,200,200,200,200,200), # n_dose
+                                            c(200,200,200,200,200,200,200,200,200,200,200),c(0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0))) # nsource (inorganic)
                                             
  # bind model estimates together
 
@@ -214,12 +239,12 @@ meta2 <- readRDS('products/nue_meta2.rds')
    se <- unlist(e2_clay.df$se, use.names=FALSE)
    
    # create vectors for the other covariables
-   xclay <- seq(5,50,5)
-   xsom <- rep(35,10)
-   tmp <- rep(15,10)
-   pre <- rep(1000,10)
-   nsource <- rep("inorganic",10)
-   ndose <- rep(200,10) 
+   xclay <- seq(5,55,5)
+   xsom <- rep(35,11)
+   tmp <- rep(15,11)
+   pre <- rep(1000,11)
+   nsource <- rep("inorganic",11)
+   ndose <- rep(200,11) 
    
    # combine extractions and vectors
    nue_e2_clay <- data.table(nue,se,cil,ciu,xclay,xsom,tmp,pre,nsource,ndose)
@@ -238,14 +263,14 @@ meta2 <- readRDS('products/nue_meta2.rds')
    
   
  # predict NUE under varying som content
- p3_som <- predict.rma(meta2, newmods=cbind(c(25,25,25,25,25,25,25,25,25,25,25,25,25,25,25),# xclay
+ p3_som <- predict.rma(meta2, newmods=cbind(c(25,25,25,25,25,25,25,25,25,25,25,25,25,25,25), # xclay
                                             c(10,20,30,40,50,60,70,80,90,100,110,120,130,140,150), # xsom
                                             c(15,15,15,15,15,15,15,15,15,15,15,15,15,15,15), # tmp_mean
                                             c(1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000,1000), # pre_mean
-                                            c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0), # nsource (inorganic)
+                                            c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0), # nsource (inorganic)
                                             c(200,200,200,200,200,200,200,200,200,200,200,200,200,200,200), # n_dose
                                             c(200,200,200,200,200,200,200,200,200,200,200,200,200,200,200), # n_dose
-                                            c(1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))) # nsource (inorganic)
+                                            c(200,200,200,200,200,200,200,200,200,200,200,200,200,200,200),c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0))) # nsource (inorganic)
                                          
    # bind model estimates together
    
@@ -288,14 +313,14 @@ meta2 <- readRDS('products/nue_meta2.rds')
        
                        
  # predict NUE under varying mean temperature 
- p4_temp <- predict.rma(meta2, newmods=cbind(c(25,25,25,25,25,25,25,25,25), # xclay
-                                             c(35,35,35,35,35,35,35,35,35), # xsom
+ p4_temp <- predict.rma(meta2, newmods=cbind(c(25,25,25,25,25,25,25,25,25,25), # xclay
+                                             c(35,35,35,35,35,35,35,35,35,35), # xsom
                                              c(3,6,9,12,15,18,21,24,27,30), # tmp_mean
                                              c(1000,1000,1000,1000,1000,1000,1000,1000,1000,1000), # pre_mean
-                                             c(1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0), # nsource (inorganic)
+                                             c(1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0), # nsource (inorganic)
                                              c(200,200,200,200,200,200,200,200,200,200), # n_dose
                                              c(200,200,200,200,200,200,200,200,200,200), # n_dose
-                                             c(1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0))) # nsource (inorganic)
+                                             c(200,200,200,200,200,200,200,200,200,200),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0))) # nsource (inorganic)
                                              
                                              
                                              
@@ -340,14 +365,14 @@ meta2 <- readRDS('products/nue_meta2.rds')
  
    
  # predict NUE under varying mean precipitation
- p5_prec <- predict.rma(meta2, newmods=cbind(c(25,25,25,25,25,25,25,25,25), # xclay
-                                             c(35,35,35,35,35,35,35,35,35), # xsom
-                                             c(15,15,15,15,15,15,15,15,15), # tmp_mean
-                                             c(1000,1000,1000,1000,1000,1000,1000,1000,1000,1000), # pre_mean
-                                             c(1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0), # nsource (inorganic)
+ p5_prec <- predict.rma(meta2, newmods=cbind(c(25,25,25,25,25,25,25,25,25,25), # xclay
+                                             c(35,35,35,35,35,35,35,35,35,35), # xsom
+                                             c(15,15,15,15,15,15,15,15,15,15), # tmp_mean
+                                             c(200,400,600,800,1000,1200,1400,1600,1800,2000), # pre_mean
+                                             c(1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0), # nsource (inorganic)
                                              c(200,200,200,200,200,200,200,200,200,200), # n_dose
                                              c(200,200,200,200,200,200,200,200,200,200), # n_dose
-                                             c(1,1,1,1,1,1,1,1,1,1),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0))) # nsource (inorganic)
+                                             c(200,200,200,200,200,200,200,200,200,200),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0),c(0,0,0,0,0,0,0,0,0,0))) # nsource (inorganic)
 
    # bind model estimates together
    
@@ -388,14 +413,6 @@ meta2 <- readRDS('products/nue_meta2.rds')
    # save plot
    #ggsave(plot = plot_nue_prec, filename = 'products/nue_prec.jpg') 
                            
- 
- 
- 
- 
- 
- 
- 
- 
  
  
  
